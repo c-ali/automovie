7704c12d2f23906c000f1b88eae76c1b838bc1ec
ChrisAli <chalimeh@uni-mainz.de>
2023-10-06 04:22:32 +0200

    initial commit

---
+++ morph.py
@@ -0,0 +1,93 @@
+# latent bleeding imports
+import torch
+torch.backends.cudnn.benchmark = False
+torch.set_grad_enabled(False)
+import warnings
+warnings.filterwarnings('ignore')
+from latent_blending import LatentBlending
+from stable_diffusion_holder import StableDiffusionHolder
+from movie_util import concatenate_movies
+from huggingface_hub import hf_hub_download
+# chatgpt imports
+import re
+import openai
+
+# Settings
+fps = 24
+num_prompts = 30
+duration_single_trans = 3
+depth_strength = 0.55  # Specifies how deep (in terms of diffusion iterations the first branching happens)
+def remove_prefixes(text):
+    # Split the text by newline
+    lines = text.split('\n')
+
+    # Use a regular expression to remove prefixes like "1.", "5.", "13.", etc.
+    cleaned_lines = [re.sub(r'^\d+\.\s*', '', line) for line in lines]
+
+    # Remove empty lines
+    cleaned_lines = [l for l in cleaned_lines if len(l) > 0]
+
+    return cleaned_lines
+
+
+theme = input("Please input the theme of the movie \n")
+openai.api_key = open("openai_apikey", "r").read()
+
+completion = openai.Completion.create(
+  model = "gpt-3.5-turbo-instruct",
+  temperature = 0.6,
+  prompt=f"Create a list of {num_prompts} descriptive prompts for an image generation AI on the theme of {theme}."
+         f" The prompts should tell a time coherent story where each image is related to the last and the next."
+         f" Separate the prompts by a newline",
+  max_tokens = 2000,
+)
+
+raw_text = completion.choices[0].text
+split_prompts = remove_prefixes(raw_text)
+print(f"Theme: {theme}. Prompts: {raw_text}. Generating movie...")
+
+
+# %% First let us spawn a stable diffusion holder. Uncomment your version of choice.
+fp_ckpt = hf_hub_download(repo_id="stabilityai/stable-diffusion-2-1-base", filename="v2-1_512-ema-pruned.ckpt")
+# fp_ckpt = hf_hub_download(repo_id="stabilityai/stable-diffusion-2-1", filename="v2-1_768-ema-pruned.ckpt")
+sdh = StableDiffusionHolder(fp_ckpt)
+lb = LatentBlending(sdh)
+
+
+
+# Specify a list of prompts below
+list_prompts = split_prompts
+
+# You can optionally specify the seeds
+#list_seeds = [954375479, 332539350, 956051013, 408831845, 250009012, 675588737]
+t_compute_max_allowed = 12  # per segment
+out_name = "out.mp4"
+
+list_movie_parts = []
+for i in range(len(list_prompts) - 1):
+    # For a multi transition we can save some computation time and recycle the latents
+    if i == 0:
+        lb.set_prompt1(list_prompts[i])
+        lb.set_prompt2(list_prompts[i + 1])
+        recycle_img1 = False
+    else:
+        lb.swap_forward()
+        lb.set_prompt2(list_prompts[i + 1])
+        recycle_img1 = True
+
+    fp_movie_part = f"tmp_part_{str(i).zfill(3)}.mp4"
+    fp_img_part = f"tmp_part_{str(i).zfill(3)}"
+    # Run latent blending
+    lb.run_transition(
+        recycle_img1 = recycle_img1,
+        depth_strength=depth_strength,
+        t_compute_max_allowed=t_compute_max_allowed
+    )
+
+    # Save movie
+    lb.write_movie_transition(fp_movie_part, duration_single_trans, fps=24)
+    lb.write_imgs_transition(fp_img_part)
+    list_movie_parts.append(fp_movie_part)
+
+# Finally, concatente the result
+concatenate_movies(out_name, list_movie_parts)
